ecea1e5edfabf7503357005f2ba8e9aad65923ba
DO <hnyhazel@gmail.com>
2018-09-19 23:39:14 -0600

    Set up app for heroku

--- nodejs/the-complete-nodejs-developer-course/node-todo-api/server/server.js
+++ nodejs/the-complete-nodejs-developer-course/node-todo-api/server/server.js
@@ -3,13 +3,12 @@ var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
-//Equivalent to var mongoose = require('./db/mongoose').mongoose
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
-//middleware
 app.use(bodyParser.json());
 
 app.get('/todos', (req, res) => {
@@ -55,8 +54,8 @@ app.post('/todos', (req, res) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000')
+app.listen(port, () => {
+  console.log(`Started at port ${port}`)
 });
 
 module.exports = { app };
