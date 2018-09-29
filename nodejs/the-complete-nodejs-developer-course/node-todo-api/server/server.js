e45c0eb4625931ced45a1df90737bddc292d5d64
DO <darya.ostapenko@gmail.com>
2018-09-29 00:22:17 -0600

    Add PATCH route /todos/:id

--- nodejs/the-complete-nodejs-developer-course/node-todo-api/server/server.js
+++ nodejs/the-complete-nodejs-developer-course/node-todo-api/server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -21,7 +22,33 @@ app.get('/todos', (req, res) => {
   })
 });
 
-// GET /todos/:id
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  //only pull off the properties you want the user to be able to update
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  //db.collection.update(query, update, options)
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(400).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+});
+
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id;
 
@@ -36,7 +63,6 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
-
 });
 
 app.post('/todos', (req, res) => {
@@ -69,6 +95,8 @@ app.delete('/todos/:id', (req, res) => {
       res.status(400).send(e);
   });
 });
+
+
 
 app.listen(port, () => {
   console.log(`Started at port ${port}`)
