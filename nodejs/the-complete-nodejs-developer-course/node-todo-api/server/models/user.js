97921d8002298d146f0ec05d9aff81471dac93f5
DO <darya.ostapenko@gmail.com>
2018-10-13 17:26:59 -0600

    Added Post /users/login and user schema method to find user by credentials

--- nodejs/the-complete-nodejs-developer-course/node-todo-api/server/models/user.js
+++ nodejs/the-complete-nodejs-developer-course/node-todo-api/server/models/user.js
@@ -50,6 +50,27 @@ var UserSchema = new Schema({
   }]
 });
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var user = this;
+  return user.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+    //bcrypt only supports callbacks, no support for Promises
+    return new Promise((resolve, reject) => {
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+
+    });
+  });
+}
+
 UserSchema.pre('save', function (next) {
   var user = this;
   //avoid hashing hashed password
