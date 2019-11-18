614c2cd56ddaaae201de41e5fc34d6e48d23316f
darya <darya@myheat.ca>
2019-11-17 23:08:12 -0700

    Added icons

--- vuejs/VueSPAwithFirebase/src/views/Home.vue
+++ vuejs/VueSPAwithFirebase/src/views/Home.vue
@@ -4,30 +4,12 @@
       <div class="row justify-content-center">
         <div class="col-10 col-md-10 col-lg-8 col-xl-7">
           <h4 class="display-4 text-primary mb-2">Meeting Log</h4>
-          <p class="lead">
-            This simple app creates meetings, allows people to check in, and
-            picks random users to award giveaways. Built with 
-            <a href="https://vuejs.org/">Vue.js</a>
-            and
-            <a href="https://firebase.google.com">Firebase</a>.
-          </p>
+          <p class="lead">This simple app creates meetings, allows people to check in, and picks random users to award giveaways. Built with <a href="https://vuejs.org/">Vue.js</a> and <a href="https://firebase.google.com">Firebase</a>.<font-awesome-icon icon="star" class="text-primary"></font-awesome-icon></p>
           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-          <router-link
-            class="btn btn-outline-primary mr-3"
-            to="/register"
-            v-if="!user"
-          >Register</router-link>
-          <router-link
-            class="btn btn-outline-primary mr-3"
-            to="/login"
-            v-if="!user"
-          >Log In</router-link>
-          <router-link
-            class="btn btn-primary"
-            to="/meetings"
-            v-if="user"
-          >Meetings</router-link>
+          <router-link class="btn btn-outline-primary mr-3" to="/register" v-if="!user">Register</Router-link>
+          <router-link class="btn btn-outline-primary mr-3" to="/login" v-if="!user">Log In</router-link>          
+          <router-link class="btn btn-primary" to="/meetings" v-if="user">Meetings</router-link>
         </div>
       </div>
     </div>
@@ -35,5 +17,11 @@
 </template>
 
 <script>
-
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+export default {
+  name: "home",
+  components: {
+    FontAwesomeIcon
+  }
+};
 </script>
