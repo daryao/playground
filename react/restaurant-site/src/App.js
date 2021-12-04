c5584c854c76784c2ef14116634da70ad07f81b4
darya <hnyhazel@gmail.com>
2021-12-03 19:32:55 -0700

    Added some state logic and icons

--- react/restaurant-site/src/App.js
+++ react/restaurant-site/src/App.js
@@ -1,18 +1,24 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSmileBeam, faFrown } from '@fortawesome/free-regular-svg-icons'
 import './App.css';
 import storefront from './assets/storefront_illustration.png'
+
+// specials
+const [,friday,,sunday] = ["sushi", "ribs", "chicken", "chicken"];
 
 function PromoCodes() {
     return <p>Current promo codes</p>
 }
 
-function Header(props) {
+function Header({name}) {
 	return (
         <div className="section">
             <div className="container">
                 <div className="row">
                     <div className="column"></div>
                         <header>
-                            <h1>{props.name}</h1>
+                            <h1>{name}</h1>
                         </header>
                     </div>
             </div>
@@ -20,7 +26,7 @@ function Header(props) {
 	);
 }
 
-function Main(props) {
+function Main({menu}) {
 	return (
         <div className="section">
             <div className="container">
@@ -29,7 +35,7 @@ function Main(props) {
                         <img className="storefront" src={storefront} alt="Illustration of a restaurant storefront" />
                         <p>Serving the most delicious fried chicken and more!</p>
                         <ol>
-                            {props.menu.map((item) => (
+                            {menu.map((item) => (
                                 <li key={item.id}>{item.title}</li>
                             ))}
                         </ol>
@@ -40,14 +46,23 @@ function Main(props) {
 	);
 }
 
-function Footer(props) {
+function Footer({year}) {
+    const [sentiment, setSentiment] = useState("positive");
     return (
         <div className="section footer">
             <div className="container">
                 <div className="row">
                     <div className="column">
                         <p>Thanks for visiting!</p>
-                        <div className="copy">Copyright © {props.year}</div>
+                        <div className="row">
+                            <div className="one-half column"><button className="button-primary" onClick={() => setSentiment("positive")}>I am happy!</button></div>
+                            <div class="one-half column"><button onClick={() => setSentiment("negative")}>No, I have issues with the site</button></div>
+                        </div>
+                        <br />
+                        <p>Current state:&nbsp;
+                            {sentiment == "positive" ? (<FontAwesomeIcon className="has-color-positive" icon={faSmileBeam} />) : (<FontAwesomeIcon className="has-color-negative" icon={faFrown} />) }
+                        </p>
+                        <div className="copy">Copyright © {year}</div>
                     </div>
                 </div>
             </div>
@@ -65,7 +80,7 @@ const menu = [
 
 const menuObjects = menu.map((item, i) => ({id: i, title : item}))
 
-function MainContent(props) {
+function MainContent() {
     return (
         <div className="App">
             <Header name="Chicken Kitchen" />
@@ -75,7 +90,7 @@ function MainContent(props) {
       );
 }
 
-function AuthContent(props) {
+function AuthContent() {
     return (
         <div className="App">
             <Header name="Chicken Kitchen" />
@@ -89,7 +104,7 @@ function AuthContent(props) {
 function App(props) {
     return (
         <>
-            {props.authorized ? <AuthContent props={props} /> : <MainContent props={props} />}
+            {props.authorized ? <AuthContent /> : <MainContent />}
         </>
     );
 }
